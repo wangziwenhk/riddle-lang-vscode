f9b0267d1285ea87bc86e77a66d11078f56a8060
wangziwenhk <wangziwen2011@icloud.com>
2025-03-02 18:34:16 +0800

    添加运算符

--- src/keywords.ts
+++ src/keywords.ts
@@ -5,124 +5,161 @@ export const KeywordCompletionItems = [
     {
         label: 'fun',
         kind: CompletionItemKind.Keyword,
-        detail: '关键字 fun'
+        detail: 'Riddle 关键字'
     },
     {
         label: 'var',
         kind: CompletionItemKind.Keyword,
-        detail: '关键字 var'
+        detail: 'Riddle 关键字'
     },
     {
         label: 'val',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 val'
+        detail: 'Riddle 关键字'
     },
     {
         label: 'return',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 return'
+        detail: 'Riddle 关键字'
     },
     {
         label: 'for',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 for'
+        detail: 'Riddle 关键字'
     },
     {
         label: 'while',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 while'
+        detail: 'Riddle 关键字'
     },
     {
         label: 'if',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 if'
-    }, 
+        detail: 'Riddle 关键字'
+    },
     {
         label: 'else',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 else'
-    }, 
+        detail: 'Riddle 关键字'
+    },
     {
         label: 'continue',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 continue'
-    }, 
+        detail: 'Riddle 关键字'
+    },
     {
         label: 'break',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 break'
+        detail: 'Riddle 关键字'
     },
     {
         label: 'import',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 import'
+        detail: 'Riddle 关键字'
     },
     {
         label: 'package',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 package'
+        detail: 'Riddle 关键字'
     },
     {
         label: 'class',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 class'
+        detail: 'Riddle 关键字'
     },
     {
         label: 'true',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 true'
+        detail: 'Riddle 关键字'
     },
     {
         label: 'false',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 false'
+        detail: 'Riddle 关键字'
     },
     {
         label: 'null',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 null'
+        detail: 'Riddle 关键字'
     },
     {
         label: 'try',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 try'
+        detail: 'Riddle 关键字'
     },
     {
         label: 'catch',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 catch'
+        detail: 'Riddle 关键字'
     },
     {
         label: 'override',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 override'
+        detail: 'Riddle 关键字'
     },
     {
         label: 'static',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 static'
+        detail: 'Riddle 关键字'
     },
     {
         label: 'const',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 const'
+        detail: 'Riddle 关键字'
     },
     {
         label: 'public',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 public'
+        detail: 'Riddle 关键字'
     },
     {
         label: 'protected',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 protected'
-    },{
+        detail: 'Riddle 关键字'
+    }, 
+    {
         label: 'private',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 private'
-    },{
+        detail: 'Riddle 关键字'
+    }, 
+    {
         label: 'virtual',
         kind: CompletionItemKind.Keyword,
-        detail: '关键词 virtual'
+        detail: 'Riddle 关键字'
     },
+    {
+        label: 'int',
+        kind: CompletionItemKind.Keyword,
+        detail: '32位整数类型'
+    },
+    {
+        label : 'long',
+        kind: CompletionItemKind.Keyword,
+        detail: '64位整数类型'
+    },
+    {
+        label: 'double',
+        kind: CompletionItemKind.Keyword,
+        detail: '双浮点类型'
+    },
+    {
+        label: 'float',
+        kind: CompletionItemKind.Keyword,
+        detail: '单浮点类型'
+    },
+    {
+        label: 'short',
+        kind: CompletionItemKind.Keyword,
+        detail: '16位整数类型'
+    },
+    {
+        label: 'char',
+        kind: CompletionItemKind.Keyword,
+        detail: '8位字符类型'
+    },
+    {
+        label: 'bool',
+        kind: CompletionItemKind.Keyword,
+        detail: '1位布尔类型'
+    }
 ];
